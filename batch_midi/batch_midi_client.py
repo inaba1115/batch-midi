06a60758a22f5741c0f42767343e1c48f0593100
inaba1115 <inaba1115@gmail.com>
2024-07-25 21:45:17 +0900

    update (#20)

--- batch_midi/batch_midi_client.py
+++ batch_midi/batch_midi_client.py
@@ -1,7 +1,7 @@
 import threading
 import typing
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import timedelta
 
 import mido  # type: ignore
 import superdirtpy as sd
@@ -18,21 +18,18 @@ class MidiEvent:
 
 
 class BatchMidiClient(sd.SuperDirtClient):
-    def __init__(self) -> None:
-        self._event_buffer: list[tuple[dict, datetime]] = []
+    def __init__(self, enable_thread_safe: bool = False) -> None:
+        self._event_buffer: list[tuple[dict, timedelta]] = []
+        self._enable_thread_safe = enable_thread_safe
+        if self._enable_thread_safe:
+            self._lock = threading.Lock()
 
-    def send(self, event: dict, timestamp: datetime, dryrun: bool = False) -> None:
-        self._event_buffer.append((event, timestamp))
-
-
-class SyncBatchMidiClient(BatchMidiClient):
-    def __init__(self) -> None:
-        super().__init__()
-        self._lock = threading.Lock()
-
-    def send(self, event: dict, timestamp: datetime, dryrun: bool = False) -> None:
-        with self._lock:
-            self._event_buffer.append((event, timestamp))
+    def send(self, tctx: sd.TemporalContext, event: dict) -> None:
+        if self._enable_thread_safe:
+            with self._lock:
+                self._event_buffer.append((event, tctx.elapsed_time()))
+        else:
+            self._event_buffer.append((event, tctx.elapsed_time()))
 
 
 def write(client: typing.Any, out_dir: str, prefix: str = "") -> None:
@@ -45,25 +42,22 @@ def write(client: typing.Any, out_dir: str, prefix: str = "") -> None:
     outfile.tracks.append(track)
 
     midi_events: list[MidiEvent] = []
-    for event, timestamp in client._event_buffer:
+    for event, elapsed_time in client._event_buffer:
         if event.get("n") is None:  # rest note
             continue
 
         octave = event.get("octave", 5)
         note = event["n"] + 12 * octave
         velo = int(127 * event.get("amp", 0.5))
-        ts = timestamp.timestamp()
+        ts = elapsed_time.total_seconds()
         sustain = event.get("sustain", event["delta"])
 
         midi_events.append(MidiEvent("note_on", note, velo, second2tick(ts)))
         midi_events.append(MidiEvent("note_off", note, velo, second2tick(ts + sustain)))
 
     on_notes: set = set()
-    now = -1
+    now = 0
     for e in sorted(midi_events, key=lambda e: e.tick):
-        if now < 0:
-            now = e.tick
-
         if e.typ == "note_on":
             if e.note in on_notes:
                 track.append(mido.Message(type="note_off", note=e.note, velocity=0, time=e.tick - now))
@@ -77,7 +71,5 @@ def write(client: typing.Any, out_dir: str, prefix: str = "") -> None:
                 on_notes.remove(e.note)
             else:
                 pass
-
         now = e.tick
-
     outfile.save(gen_filename(out_dir, prefix))
